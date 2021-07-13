5290114f7711eda0c63321dde170be5b40ce927c
Naoroy <fegan.pierre@gmail.com>
2021-07-13 17:34:11 +0200

    Add form to create task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,64 +1,98 @@
 import React from 'react';
 import './Task.css';
+const API_URL = 'http://localhost:1337/tasks'
 
-const TaskComponent = function() {
-  return (<div id="task-container">
-    <TaskHeader />
-    <TaskList />
-  </div>)
-}
-
-const TaskHeader = function() {
-  return (<div id="task-header">
-  </div>)
-}
-
-class TaskList extends React.Component {
+class TaskComponent extends React.Component {
   constructor() {
-    super();
+    super()
+
     this.state = {
       tasks: [],
-      status: true
     };
-
+    this.submitTaskHandler = this.submitTaskHandler.bind(this)
     this.getTasks()
-      .then((tasks) => {
-        this.setState({ tasks })
-      });
+      .then((tasks) => { this.setState({ tasks }) });
   }
 
   render() {
-    return (<div id="task-list">
-        {
-          this.state.tasks.map((task, id) => {
-            return <Box key={task.name} task={task} />
-          })
-        }
-      </div>);
+    return (<div id="task-container">
+      <TaskHeader submitTaskHandler={this.submitTaskHandler}/>
+      <TaskList tasks={this.state.tasks}/>
+    </div>)
   }
 
   getTasks() {
     return new Promise((fulfill, reject) => {
-      fetch('http://localhost:1337/tasks')
+      fetch(API_URL)
         .then((res) => res.json())
         .then(fulfill)
         .catch(reject);
     });
   }
+
+  submitTaskHandler(e) {
+    e.preventDefault();
+    const form = e.target;
+    const data = new FormData(form);
+
+    fetch(API_URL, {
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      method: 'POST',
+      body: JSON.stringify(Object.fromEntries(data))
+    })
+      .then(response => response.json())
+      .then(() => {
+        this.getTasks()
+          .then((tasks) => { this.setState({ tasks }) });
+      })
+      .catch(console.log)
+  }
 }
 
-const Box = function (props) {
-  let task = props.task;
+const TaskHeader = function(props) {
+  function taskFormToggle() {
+    document.getElementById('task-form').classList.toggle('hidden');
+  }
 
-  return (<div className="task">
-    <p className="row-description"> {task.name} </p>
-    <button className="row-description" >
-      {task.done ? "Done" : "On going"}
+  return (<div id="task-header">
+    <button
+      className="action"
+      onClick={taskFormToggle}
+    >
+      New task
     </button>
-    <p className="row-description"> {task.description} </p>
-    <p className="row-description"> {task.date_due} </p>
-    <p className="row-description"> {task.time_spent} </p>
+    <form
+      id="task-form" className="hidden"
+      onSubmit={(e) => {
+        props.submitTaskHandler(e)
+      }}
+    >
+      <label htmlFor="name">Task name</label>
+      <input type="text" name="name"/>
+      <input type="submit" value="Create"/>
+    </form>
   </div>);
+}
+
+const TaskList = function(props) {
+  const tasks = props.tasks;
+  const desc = (a, b) => b.id - a.id;
+
+  return (
+    <div id="task-list">
+      {tasks.sort(desc).map((task, id) => {
+        return (<div key={task.id} className="task">
+          <p className="row-description"> {task.name} </p>
+          <button className="row-description" >
+            {task.done ? "Done" : "On going"}
+          </button>
+          <p className="row-description"> {task.description} </p>
+          <p className="row-description"> {task.date_due} </p>
+          <p className="row-description"> {task.time_spent} </p>
+        </div>);
+      })}
+    </div>
+  );
 }
 
 export default TaskComponent;
